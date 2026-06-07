a0939932b24502cfcd2d9caf13e0193a0ae1a38c
Kim JinSIk <xhrlehgus1234@naver.com>
2026-06-07 14:42:41 +0900

    Add login related backend

--- frontend/app/useNavController.js
+++ frontend/app/useNavController.js
@@ -3,8 +3,9 @@ import { useCallback } from "react";
 import { useStore } from "./AppStore";
 
 export function useNavController() {
-  const { dispatch } = useStore();
+  const { dispatch } = useStore(); // useStore에서 dispatch만을 가져와 저장함
 
+  // 바뀐 페이지에 맞게 dispatch를 해서, goTo에 담아 반환함
   const goTo = useCallback(
     (view) => dispatch({ type: "SET_VIEW", payload: view }),
     [dispatch],
